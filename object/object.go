a6c2e5410c6526df345478be3b7c673779bc4356
Breno Silva <brenophp@gmail.com>
2024-06-15 22:46:31 +0200

    feat(object): add inspect for null type

--- object/object.go
+++ object/object.go
@@ -30,3 +30,4 @@ func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
 type Null struct {}
 
 func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string { return "null" }
